31c4546ed71b2b17a7086cff97871d3348467847
Cristhian Zepeda <criss.zep@gmail.com>
2023-02-02 23:39:53 -0300

    Landing Page - CZ

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let carrera = ["Derecho", "Analista programador"];
+let anio = [2013, 2022];
+
+let education = [];
+
+for (let i = 0; i < carrera.length; i++) {
+  education.push(`${anio[i]} ${carrera[i]}`);
+}
+
+education.forEach(console.log);
+
+console.log(education);
