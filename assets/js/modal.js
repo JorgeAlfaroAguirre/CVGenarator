5c327d885158feaadff682c35b05e9110234eee0
Cristhian Zepeda <criss.zep@gmail.com>
2023-02-04 18:49:40 -0300

    Control de mensajes de error e inputs vacios al cerrar y abrir los modals, Modal registro listo - CZ

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,22 +1,46 @@
 const login_modal = document.querySelector(".modal__login");
+const register_modal = document.querySelector(".modal__register");
 const btn_login_modal = document.querySelector(".login__login");
+const btn_register_modal = document.querySelector(".register__register");
 const btn_close__login_modal = document.querySelector(".btn__close_login");
+const btn_close__register_modal = document.querySelector(
+  ".btn__close_register"
+);
 const main__background = document.querySelector(".main__background");
-const inputs = document.querySelectorAll(".input");
+const inputs = document.querySelectorAll(".input__login");
+const inputs__register = document.querySelectorAll(".input__register");
 const btn_login_confirmar = document.querySelector(".btn__login");
+const btn_register_confirmar = document.querySelector(
+  ".btn_confirmar_register"
+);
 const msg_error_modal = document.querySelector(".msg__login_error");
+const msg_error_modal_register = document.querySelector(".msg__register_error");
+
+const clearInput = (inputs) => {
+  inputs.forEach((input) => {
+    input.value = "";
+  });
+};
+
+/********** LOGIN **************/
 
 btn_login_modal.addEventListener("click", () => {
   main__background.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
   login_modal.style.display = "block";
+  register_modal.style.display = "none";
+  msg_error_modal_register.style.display = "none";
+  msg_error_modal.style.display = "none";
 });
 
 btn_close__login_modal.addEventListener("click", () => {
-  inputs.forEach((input) => {
+  /* inputs.forEach((input) => {
     input.value = "";
-  });
+  }); */
+  clearInput(inputs);
+  clearInput(inputs__register);
   main__background.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
   login_modal.style.display = "none";
+  msg_error_modal_register.style.display = "none";
   msg_error_modal.style.display = "none";
 });
 
@@ -27,5 +51,36 @@ btn_login_confirmar.addEventListener("click", () => {
     msg_error_modal.style.display = "block";
   } else {
     msg_error_modal.style.display = "none";
+  }
+});
+
+/*****************  REGISTER ********************/
+
+btn_register_modal.addEventListener("click", () => {
+  main__background.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+  register_modal.style.display = "block";
+  login_modal.style.display = "none";
+  msg_error_modal_register.style.display = "none";
+  msg_error_modal.style.display = "none";
+});
+
+btn_close__register_modal.addEventListener("click", () => {
+  clearInput(inputs);
+  clearInput(inputs__register);
+  main__background.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+  register_modal.style.display = "none";
+  msg_error_modal_register.style.display = "none";
+  msg_error_modal.style.display = "none";
+});
+
+btn_register_confirmar.addEventListener("click", () => {
+  let allInputsFilled = Array.from(inputs__register).every(
+    (input) => input.value !== ""
+  );
+  console.log(allInputsFilled);
+  if (!allInputsFilled) {
+    msg_error_modal_register.style.display = "block";
+  } else {
+    msg_error_modal_register.style.display = "none";
   }
 });
